46a55dcdee331b206b51c843ff798d34469708cc
noboru-i <ishikura.noboru@gmail.com>
2018-05-02 21:57:44 +0900

    fix format

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var googlehome = require('google-home-notifier');
-var language = 'ja';
+const googlehome = require('google-home-notifier');
 
-googlehome.device('Google-Home-Mini-f51f9b7b03f902ceb33d1a104af58598', language);
+const LANGUAGE = 'ja';
+const DEVICES = {
+  DINING: 'Google-Home-Mini-f51f9b7b03f902ceb33d1a104af58598',
+};
 
-googlehome.notify('明日は、燃えるゴミの日です。', function(res) {
+googlehome.device(DEVICES.DINING, LANGUAGE);
+googlehome.notify('明日は、燃えるゴミの日です。', (res) => {
   console.log(res);
 });
